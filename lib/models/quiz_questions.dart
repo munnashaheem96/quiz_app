fad114c15846370833d67d4d17ac546c26f48063
Munna Shaheem <munnashaheemperinchikkal@gmail.com>
2023-07-29 16:04:58 +0530

    cmtd 29

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -3,4 +3,11 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  // to shuffle answers step1
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+
+// ignore: use_key_in_widget_constructors
 class QuestionsScreen extends StatefulWidget {
   @override
   State<QuestionsScreen> createState() {
@@ -8,8 +12,64 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              //shuffle step2 - add getShuffledAnswers()
+              return AnswerButton(
+                answerText: answer,
+                onTap: answerQuestion,
+              );
+            }),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[0],
+            //   onTap: () {}
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[1],
+            //   onTap: () {}
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[2],
+            //   onTap: () {}
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[3],
+            //   onTap: () {}
+            // ),
+          ],
+        ),
+      ),
+    );
   }
 }
